8cba043b65457cf9133fb64fcaf921b07ca53889
Gautierpicon <gautierpicon@proton.me>
2025-12-26 18:52:59 +0100

    style: add SVGs insteed of emojis

--- src/routes/store.js
+++ src/routes/store.js
@@ -1,3 +1,11 @@
 import { writable } from 'svelte/store';
 
 export const useStar = writable(false);
+
+if (typeof window !== 'undefined') {
+	useStar.set(localStorage.getItem('useStar') === 'true');
+
+	useStar.subscribe((value) => {
+		localStorage.setItem('useStar', value);
+	});
+}
